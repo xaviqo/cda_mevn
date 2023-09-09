024e1d109fb1ebe5f64543ea5f67ca714129df98
xavi debian <mail@xavi.tech>
2023-09-09 17:17:01 +0200

    added logger to trace errors

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,31 +2,47 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const winston = require('winston');
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const moment = require('moment');
 require('dotenv').config();
 const app = express();
 const loginService = require('./service/login.service');
 
-
 app.use(cors({
-    origin: 'http://localhost:8080'
+    origin: 'https://cda.xavi.tech'
 }));
 
+const loggerTime = moment().format('YYYY-MM-DD_HH-mm-ss');
+const logFileName = `CDA_${timestamp}.log`;
 
-/*
-app.use(cors({
-    origin: 'http://130.162.47.190'
-}))
-*/
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.printf(({ timestamp, level, message }) => {
+        return `[CDA] ${timestamp} - [${level}]: ${message}`;
+      })
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: `${logFileName}.log` }),
+    ],
+  });
+  
 
-// Settings
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI,{
+    keepAlive: true,
+    keepAliveInitialDelay: 300000, 
+    })
     .then(db => console.log('[CDA] MongoDB: connected'))
-    .catch(err => console.log(err));
+    .catch(err => {
+        console.error('[CDA] MongoDB connection error:', err);
+    });
 
 ControllerRoute = require('./controller/controller');
 app.use('/api', ControllerRoute);
