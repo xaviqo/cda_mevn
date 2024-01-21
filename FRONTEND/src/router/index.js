ee16a1a34b4a76cb45e532f48f6708498c92b575
Xavi Quinones <mail@xavi.tech>
2024-01-21 21:43:29 +0100

    Added custom page titles @ vue-router

--- FRONTEND/src/router/index.js
+++ FRONTEND/src/router/index.js
@@ -5,31 +5,43 @@ import AdminLogin from "@/views/AdminLogin.vue";
 import AdminPanel from "@/views/AdminPanel.vue";
 import ActorProfile from "@/views/ActorProfile.vue";
 
+const CDA_TITLE = "Carrusel de Actores"
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: "Representación de actores y actrices " + new Date().getFullYear()
+    }
   },
   {
     path: '/portfolio/:name/:section?',
     name: 'profile',
-    component: ActorProfile
+    component: ActorProfile,
+    meta: {
+      title: "Perfil de "
+    }
   },
   {
     path: '/cda-admin',
     name: 'admin-panel',
     meta: {
-      admin: true
+      admin: true,
+      title: "Panel de Admin CDA"
     },
     component: AdminPanel
   },
   {
     path: '/cda-admin/login',
     name: 'admin-login',
-    component: AdminLogin
+    component: AdminLogin,
+    meta: {
+      title: "Login CDA"
+    }
   }
 ]
 
@@ -40,6 +52,15 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
+  if (to.meta.title.toUpperCase().includes("PERFIL")) {
+    document.title = CDA_TITLE + " - " + to.meta.title + paramCleaner(to.params.name)
+    if (to.params.section) {
+          document.title = document.title + " " + paramCleaner(to.params.section)
+    }
+  } else {
+    document.title = CDA_TITLE + " - " + to.meta.title;
+  }
+  document.title = document.title + " " + new Date().getFullYear();
   const ls = JSON.parse(localStorage.getItem('cda_session'));
   if (to.meta.admin){
     if (ls != null && ls['token'] && ls['id']) next();
@@ -47,5 +68,13 @@ router.beforeEach((to, from, next) => {
   }
   next();
 });
+
+const paramCleaner = (param) => {
+  return param.includes("-")
+      ? param.split('-')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() )
+          .join(' ')
+      : param.charAt(0).toUpperCase() + param.slice(1).toLowerCase();
+}
 
 export default router;
